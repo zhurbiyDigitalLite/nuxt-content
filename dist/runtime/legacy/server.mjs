a145adf3d5330aff678ca77d6433484bd7e37887
zhurbiy_digitalLite <zhurbii.m@digitalite.org>
2024-01-03 14:41:23 +0200

    init files

---
+++ dist/runtime/legacy/server.mjs
@@ -0,0 +1,3 @@
+import { serverQueryContent as _serverQueryContent } from "../server/storage.mjs";
+export { parseContent } from "../server/storage.mjs";
+export const serverQueryContent = _serverQueryContent;
---
+++ dist/runtime/server/api/query.mjs
@@ -0,0 +1,35 @@
+import { createError } from "h3";
+import { serverQueryContent } from "../storage.mjs";
+import { getContentQuery } from "../../utils/query.mjs";
+import { useRuntimeConfig, cachedEventHandler } from "#imports";
+export default cachedEventHandler(async (event) => {
+  const query = getContentQuery(event);
+  const { advanceQuery } = useRuntimeConfig().public.content.experimental;
+  if (query.first) {
+    let contentQuery = serverQueryContent(event, query);
+    if (!advanceQuery) {
+      contentQuery = contentQuery.withDirConfig();
+    }
+    const content = await contentQuery.findOne();
+    const _result = advanceQuery ? content?.result : content;
+    const missing = !_result && !content?.dirConfig?.navigation?.redirect && !content?._dir?.navigation?.redirect;
+    if (missing) {
+      throw createError({
+        statusMessage: "Document not found!",
+        statusCode: 404,
+        data: {
+          description: "Could not find document for the given query.",
+          query
+        }
+      });
+    }
+    return content;
+  }
+  if (query.count) {
+    return serverQueryContent(event, query).count();
+  }
+  return serverQueryContent(event, query).find();
+}, {
+  maxAge: 31536e3,
+  shouldBypassCache: () => !!import.meta.dev
+});
